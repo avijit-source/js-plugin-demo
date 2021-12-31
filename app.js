c755fc69e3bb805e3464c336e2c20e97dae8c8c2
Avijit Chakraborty <avijitch123@outlook.com>
2021-12-31 14:04:03 +0530

    second commit

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ function ShowAd(options){
         container: "video-div",
         videoId: "video-1",
         adShowTimer:10,
-        goto:"https://.google.com"
+        goto:"https://google.com"
     }
     options = {...defaultOptions,...options};
     let adImage = document.createElement("img");
     adImage.src = "https://i.ibb.co/JjNsyft/one-removebg-preview.png";
-    adImage.setAttribute("id","ad-image")
+    adImage.setAttribute("id","ad-image");
+    let styles = `
+    width: 100px; height: 100px;
+    z-index: 9999;
+    position: absolute;
+    top:58%;
+    left:200px;
+    `
+    adImage.style = styles;
+    document.getElementById("video-div").style.position = "relative";
     
     let videoNode = document.getElementById(options.videoId)
     var playTime = null;
@@ -20,12 +29,18 @@ function ShowAd(options){
             document.getElementById(options.container).appendChild(adImage);
             document.getElementById("ad-image").addEventListener("click",openwindow)
             function openwindow(){
-                window.open(options.goto, "_blank")
+                window.open(options.goto, "_blank");
+                videoNode.pause();
             }
-           }else if(playTime > options.adShowTimer){
-               document.getElementById("ad-image").remove();
-               clearInterval(videoInterval);
+           
+           }else if( !videoNode.paused && playTime > options.adShowTimer ){
+              
+               document.getElementById("ad-image")?.remove();
+            //    clearInterval(videoInterval);
+           }else if(playTime < options.adShow){
+               console.log("less");
            }
        }
     },1000)
+    videoInterval
 }
